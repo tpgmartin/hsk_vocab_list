f3fd4be4eb22828101bd26ed453beabc0cdd50e9
Tom Martin <tom@tpgmartin.com>
2017-01-29 00:38:05 +0000

    Basic script run on page

---
+++ index.js
@@ -0,0 +1,26 @@
+// url used http://www.hsk.academy/en/hsk_1
+
+$table = $('#flat_list');
+
+$rows = $table.find('tr');
+
+words = [];
+
+$rows.each(function (row) {
+	  var chinese = $($($rows[row]).find('td')[0]).text().split('\n');
+	    var hanzi = chinese[1].replace(/ /g, '');
+	      var pinyin = chinese[2].replace(/ /g, '');
+	        var english = $($($rows[row]).find('td')[1]).text();
+		  words.push({
+			              'hanzi': hanzi,
+			              'pinyin': pinyin,
+			              'english': english
+			              });
+});
+
+for (var i=0; i<words.length; i++) {
+	  console.log(words[i].hanzi);
+	    console.log(words[i].pinyin);
+	      console.log(words[i].english);
+}
+
